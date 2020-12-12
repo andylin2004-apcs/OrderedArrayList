fc2fc4046a9aa6678aceb5ac7f50ff48e19f8033
Andy Lin <alin21@stuy.edu>
2020-12-11 21:06:41 -0500

    added constructors

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,3 +1,12 @@
 public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T>{
 
+  NoNullArrayList<T> orderedList;
+
+  public OrderedArrayList(){
+    nullTheNull = new NoNullArrayList<T>();
+  }
+
+  public OrderedArrayList(int initalCapacity){
+    nullTheNull = new NoNullArrayList<T>(initalCapacity);
+  }
 }
