a3dda6c8745d49dbb76b34c293948c03a1e519ec
Andy Lin <alin21@stuy.edu>
2020-12-11 22:02:50 -0500

    added add

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,12 +1,25 @@
 public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T>{
 
-  NoNullArrayList<T> orderedList;
+  OrderedArrayList<T> orderedList;
 
   public OrderedArrayList(){
-    nullTheNull = new NoNullArrayList<T>();
+    orderedList = new OrderedArrayList<T>();
   }
 
   public OrderedArrayList(int initalCapacity){
-    nullTheNull = new NoNullArrayList<T>(initalCapacity);
+    orderedList = new OrderedArrayList<T>(initalCapacity);
+  }
+
+  public boolean add(T value){
+    if (value == null){
+      throw new IllegalArgumentException();
+    }
+    for (int i = 0; i<orderedList.size(); i++){
+      if (orderedList.get(i).compareTo(value) < 0){
+        orderedList.add(i, value);
+        return true;
+      }
+    }
+    return true;
   }
 }
